43cedccca180c1d1787be97cc46c61a488e0acd0
murphypicard <ayaghsizian@gmail.com>
2017-02-08 18:23:30 -0500

    still working on navbar 6pm wednesday

--- js/dancers.controller.js
+++ js/dancers.controller.js
@@ -3,14 +3,24 @@
   .module('dancersModule')
   .controller('dancersCTRL', dancersCTRLfunction)
 
-  // FoodCtrl.$inject = ['$http', '$scope', 'FoodFactory'];
+  dancersCTRLfunction.$inject = ['$http', '$scope', 'DancerFactory'];
 
-  function dancersCTRLfunction(){
+  // function MoodCtrl($http, $scope, MoodFactory) {
+  function dancersCTRLfunction($http, $scope, DancerFactory){
     this.dancers = [
       {firstName: 'Abraham', lastName: 'Lincoln'},
       {firstName: 'George', lastName: 'Bush'}
     ];
-  }
+
+    $scope.getDancers = function(){
+      DancerFactory.get()
+      .then(function(res){
+
+      }
+
+    }//get
+
+  }//dancersCTRL
 
 
 })();
