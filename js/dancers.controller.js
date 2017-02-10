ab0bda60851f985408a27851e9ddb51f33f73adb
murphypicard <ayaghsizian@gmail.com>
2017-02-10 18:11:20 -0500

    form is working and emailing correctly

--- js/dancers.controller.js
+++ js/dancers.controller.js
@@ -3,10 +3,47 @@
   .module('dancersModule')
   .controller('dancersCTRL', dancersCTRLfunction);
 
+  angular
+  .module('dancersModule')
+  .controller('submitController', submitCTRLfunction);
+
+
+
   dancersCTRLfunction.$inject = ['$http', '$scope', 'DancerFactory'];
+  submitCTRLfunction.$inject = ['$http', '$scope'];
 
   // function MoodCtrl($http, $scope, MoodFactory) {
+  function submitCTRLfunction($http, $scope){
+    $scope.formData = {email: 'youremail@gmail.com'};
+    console.log('http');
+    console.log($http);
+    console.log('scope.formdata');
+    console.log($scope.formData);
+    $scope.text = 'Thank you for submitting.  Expect a friendly email soon.';
+    $scope.submit = function() {
+      if ($scope.text) {
+        $http({
+          method : 'POST',
+          // action="http://localhost:3001/api/dancers" method="POST"
+          url    : 'http://localhost:3001/api/dancers',
+          data   : $scope.formData,
+          // headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+          headers: {'Content-Type': 'application/json'}
 
+        })
+        .then(function successCallback(response){
+          console.log('success:::');
+          console.log(response);
+        }, function errorCallback(response){
+          console.log('error:::');
+          console.log(response);
+        }
+      );//then
+        // $scope.list.push(this.text);
+        // $scope.text = '';
+      }//if
+    }//submit
+    }//submitCTRLfunction
 
 
   function dancersCTRLfunction($http, $scope, DancerFactory){
@@ -27,6 +64,7 @@
                     if(err)console.log(err);
                   });
       };//getDancers
+
 
   }//dancersCTRL
 
