ef2f0ca9028da2d32c2199ba8a1c0d088e9e1405
murphypicard <ayaghsizian@gmail.com>
2017-02-12 14:17:59 -0500

    form submit message and paypal pic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log("JS is linked");
+
+
+
+
+function clicker(messageTag){
+  document.getElementById(messageTag).innerHTML = "Thank you for signing up!  We'll send you a confirmation email shortly.";
+  setTimeout("formSubmitter()",3000);
+}
+
+function formSubmitter(formTag){
+  document.forms[formTag].submit();
+}
